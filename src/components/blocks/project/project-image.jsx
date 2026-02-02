fa3e12aac17efcd982c76ac77f3ea867242c7062
jozin47 <josin@intersmart.in>
2026-02-02 10:30:14 +0530

    wasso jan

--- src/components/blocks/project/project-image.jsx
+++ src/components/blocks/project/project-image.jsx
@@ -5,7 +5,7 @@ export default function ProjectImage({ data, locale }) {
     <section className="w-full h-auto block">
       <Image
         src={"/images/st4.png"}
-        alt={"project"}
+        alt={"project premium"}
         width={1920}
         height={736}
         className="w-full h-full object-cover"
